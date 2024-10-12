7b018dbbaa8da3ad6053a34e755f34d031c95c5f
Ville Ranki <ville.ranki@iki.fi>
2024-10-12 16:36:43 +0300

    Tuotu alttien livekoodauskorjaukset

--- fediphoto.py
+++ fediphoto.py
@@ -10,50 +10,74 @@ from PIL import ImageFont
 import os
 import time
 
-feed = feedparser.parse('https://sauna.social/tags/altparty.rss')
+feedurls = ['https://infosec.exchange/tags/altparty.rss', 'https://sauna.social/tags/altparty.rss', 'https://mastodon.social/tags/altparty.rss']
+
+printername = 'UP-DR200'
 
 conn = cups.Connection()
 printer = conn.getDefault()
 
+os.system('lprm')
+time.sleep(1)
+os.system('cupsenable ' + printername)
+
 if not printer:
 	print('No default printer defined, cannot print!')
 
-for item in feed.entries:
-	tootid = int(item.id[item.id.rfind('/') + 1:])
-	baseurl = item.id[:item.id.rfind('@') - 1]
-	mastodon = Mastodon(api_base_url = baseurl)
-	status = mastodon.status(tootid)
-	url = status['account']['url']
-	domain = url[:url.rfind('/')][url.find('//')+2:]
-	user = url[url.rfind('@'):]
-	fediuser = user + '@' + domain
-#	print(fediuser, status['account']['display_name'], status['account']['url'])
-	for attachment in status['media_attachments']:
-		print(attachment['id'], attachment['url'])
-		filename = str(attachment['id']) + '.jpg'
-		needs_print = True
-		if Path('pics/' + filename).is_file():
-			print('(Already exists)')
-		elif Path('pics/printed/' + filename).is_file():
-			print('(Already printed)')
-			needs_print = False
-		else:
-			urlretrieve(attachment['url'], 'pics/' + filename)
-			print('Saved as', filename)
-			label = status['account']['display_name'] + '\n' + fediuser
-			img = Image.open('pics/' + filename)
-			draw = ImageDraw.Draw(img)
-			topaz = ImageFont.truetype('topaz_unicode_ks13_bold.ttf', img.size[0] / 50)
-			draw.text((img.size[0] / 30, img.size[1] / 30), label, fill=(255,255,255), stroke_fill=(0,0,0), stroke_width=5, font=topaz)
+def poll_feed(feedurl):
+	feed = feedparser.parse(feedurl)
+	for item in feed.entries:
+		tootid = int(item.id[item.id.rfind('/') + 1:])
+		baseurl = item.id[:item.id.rfind('@') - 1]
+		mastodon = Mastodon(api_base_url = baseurl)
+		status = mastodon.status(tootid)
+		url = status['account']['url']
+		domain = url[:url.rfind('/')][url.find('//')+2:]
+		user = url[url.rfind('@'):]
+		fediuser = user + '@' + domain
+
+		for attachment in status['media_attachments']:
+			print('Attachment', attachment['id'], attachment['url'], end=' -> ')
+			filename = str(attachment['id']) + '.jpg'
 			modfile = 'pics/' + 'mod_' + filename
-			img.save(modfile)
+			needs_print = True
+			needs_dl = True
+			if not '.jpg' in attachment['url']:
+				print('not .jpg')
+				needs_print = False
+				needs_dl = False
+			if Path('pics/printed/' + filename).is_file():
+				print('already printed')
+				needs_print = False
+				needs_dl = False
+			elif Path('pics/' + filename).is_file():
+				print('file exists')
+				needs_dl = False
+			if needs_dl:
+				urlretrieve(attachment['url'], 'pics/' + filename)
+				print('saved as', filename)
+			if needs_print:
+				label = status['account']['display_name'] + '\n' + fediuser
+				img = Image.open('pics/' + filename)
+				draw = ImageDraw.Draw(img)
+				topaz = ImageFont.truetype('topaz_unicode_ks13_regular.ttf', int(img.size[0] / 30))
+				draw.text((img.size[0] / 30, img.size[1] / 30), label, fill=(255,255,255), stroke_fill=(0,0,0), stroke_width=5, font=topaz)
+				img.save(modfile)
 
 			if printer and needs_print:
 				pid = conn.printFile(printer, modfile, 'From fediverse - ' + fediuser, {'fit-to-page': 'TRUE' })
-				while conn.getJobs().get(pid, None) is not None:
-					time.sleep(5)
-					print('Printing job id', pid, '..')
+				print('Printing image as job', pid, '..')
+				time.sleep(20)
+				os.system('lprm')
+				time.sleep(1)
+				os.system('cupsenable ' + printername)
 				os.rename(modfile, 'pics/printed/' + filename)
 				print('Printed and moved to ', 'pics/printed/' + filename)
 
+
+while True:
+	for feedurl in feedurls:
+		poll_feed(feedurl)
+	print('Sleeping for 60 secs, press ctrl-c to exit..')
+	time.sleep(60)
 
